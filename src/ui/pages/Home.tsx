a8fafb45ed6f692e1f6b0bd528aac6e235bf3ee7
Roberto Hernandez <roberthb94@gmail.com>
2025-02-23 15:42:50 -0600

    feat: add Home page component

---
+++ src/ui/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className='h-full flex flex-col items-center text-center'>
+      <h1 className='my-6 text-5xl font-extrabold'>Home Page</h1>
+    </div>
+  );
+};
+
+export default Home;
