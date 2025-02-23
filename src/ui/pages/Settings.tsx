6370b9ba171ae7471c7ec85b1bfe549ee482b3fb
Roberto Hernandez <roberthb94@gmail.com>
2025-02-23 15:43:31 -0600

    feat: add Settings component

---
+++ src/ui/pages/Settings.tsx
@@ -0,0 +1,9 @@
+const Settings = () => {
+  return (
+    <div className='h-full flex flex-col items-center text-center'>
+      <h1 className='my-6 text-5xl font-extrabold'>Settings Page</h1>
+    </div>
+  );
+};
+
+export default Settings;
