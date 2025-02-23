8bc133572966d058367539fc470f5571e5e966c6
Roberto Hernandez <roberthb94@gmail.com>
2025-02-23 15:43:24 -0600

    feat: add Profile component

---
+++ src/ui/pages/Profile.tsx
@@ -0,0 +1,9 @@
+const Profile = () => {
+  return (
+    <div className='h-full flex flex-col items-center text-center'>
+      <h1 className='my-6 text-5xl font-extrabold'>Profile Page</h1>
+    </div>
+  );
+};
+
+export default Profile;
