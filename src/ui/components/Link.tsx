091871f8d124d579fad461806903ae86d6775319
Roberto Hernandez <roberthb94@gmail.com>
2025-03-07 18:17:25 -0600

    feat: add Link component

---
+++ src/ui/components/Link.tsx
@@ -0,0 +1,39 @@
+import classNames from "classnames";
+import { ReactNode } from 'react';
+
+type DefaultProps = React.AnchorHTMLAttributes<HTMLAnchorElement> & {
+  children?: ReactNode;
+  color?:
+    | "vanilla"
+    | "peach"
+    | "melon"
+    | "lavender-pink"
+    | "lilac"
+    | "cornflower"
+    | "sky"
+    | "turquoise"
+    | "mint"
+    | "light-green";
+  className?: string;
+  external?: boolean;
+};
+
+const Link = ({className, external = false, ...props}: DefaultProps) => {
+
+  const handleLinkClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    window.utils.openExternalLink(event.currentTarget.href);
+  };
+
+  return (
+    <a
+      className={classNames('ml-2 underline', className)}
+      {...(external && { onClick: handleLinkClick })}
+      {...props}
+    >
+      Coda account settings
+    </a>
+  );
+};
+
+export default Link;
