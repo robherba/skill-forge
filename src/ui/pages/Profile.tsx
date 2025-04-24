b5eb487640adf1e85af91d811eec024e25e285b5
Roberto Hernandez <roberthb94@gmail.com>
2025-04-23 21:26:40 -0600

    chore: update profile page

--- src/ui/pages/Profile.tsx
+++ src/ui/pages/Profile.tsx
@@ -1,7 +1,12 @@
+import useAuthContext from '../hooks/use-auth-context';
+
 const Profile = () => {
+  const { user: userData } = useAuthContext();
+
   return (
     <div className='h-full flex flex-col items-center text-center'>
-      <h1 className='my-6 text-5xl font-extrabold'>Profile Page</h1>
+      <img src={userData?.picture || ''} className='w-20 h-20 rounded-full border-[var(--border)] border-2 shadow-[4px_4px_0px_black]' />
+      <h1 className='my-6 text-4xl font-extrabold'>{userData?.name}</h1>
     </div>
   );
 };
